106c4ca344f066fd9632ed03cba2073aebf20deb
Jhamilchar <jhamil.arroyo.a@gmail.com>
2022-01-31 23:57:06 -0500

    Arreglado de espaciados en el form

---
+++ js-function/scripts.js
@@ -0,0 +1,9 @@
+let menu = document.querySelector("#container-navigation");
+
+let menu_bar = document.querySelector("#menu-bar");
+
+    menu_bar.addEventListener("click", function() {
+
+        menu.classList.toggle("icon-icon-hamburguer-button");
+
+    })
